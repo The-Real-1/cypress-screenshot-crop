c98b327b812ba4e2874ce9dd7c9722a7bde70761
The-Real-1 <143707078+The-Real-1@users.noreply.github.com>
2023-09-01 17:03:40 +0200

    Update dropdown.cy.js

--- cypress/e2e/dropdown.cy.js
+++ cypress/e2e/dropdown.cy.js
@@ -1,14 +1,19 @@
 
 describe('Screenshot call', () => {
   it('Image croped',  () => {
+      // Could use this adress for a complete example 
       // cy.visit('https://www.primefaces.org/primeng-v15-lts/dropdown');
-      cy.visit('https://f7fpx4.sse.codesandbox.io/');
+      
+      // Code base address
+      //cy.visit('https://f7fpx4.sse.codesandbox.io/');
+      
+      // Local address available in Github repo : https://github.com/The-Real-1/cypress-screenshot-host/tree/main 
+      // To clone with git : https://github.com/The-Real-1/cypress-screenshot-host.git
+      cy.visit('http://localhost:4200');
+
       cy.get('dropdown-basic-demo p-dropdown span.p-element.p-dropdown-label').then(($el) => {
         expect($el[0].innerText).to.equal("New York");
       });
       cy.get('dropdown-basic-demo p-dropdown').screenshot();
   });
-
-  
-  
 });
